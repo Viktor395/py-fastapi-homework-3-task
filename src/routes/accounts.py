01463596deb52c4f4c502cc17a393903792008cc
Viktor Velychkocd <velviktor831@gmail.com>
2026-07-11 13:06:25 +0300

    solution1

--- src/routes/accounts.py
+++ src/routes/accounts.py
@@ -5,7 +5,7 @@ from fastapi import APIRouter, Depends, status, HTTPException
 from sqlalchemy import select, delete
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.orm import Session, joinedload
+from sqlalchemy.orm import joinedload
 
 from config import get_jwt_auth_manager, get_settings, BaseAppSettings
 from database import (
@@ -207,7 +207,7 @@ async def reset_password_complete(
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid email or token.")
 
     try:
-        user.password = user.set_password(reset_data.password)
+        user.set_password(reset_data.password)
         await db.delete(token_record)
         await db.commit()
     except SQLAlchemyError:
