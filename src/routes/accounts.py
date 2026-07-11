c7649745e0984f01fa844faef6442999073fac04
Viktor Velychkocd <velviktor831@gmail.com>
2026-07-11 12:56:24 +0300

    solution

--- src/routes/accounts.py
+++ src/routes/accounts.py
@@ -207,7 +207,7 @@ async def reset_password_complete(
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid email or token.")
 
     try:
-        user.password = reset_data.password
+        user.password = user.set_password(reset_data.password)
         await db.delete(token_record)
         await db.commit()
     except SQLAlchemyError:
@@ -272,7 +272,7 @@ async def login_user(
 
 
 @router.post(
-    "/refresh/",
+    "/api/v1/accounts/refresh/",
     response_model=TokenRefreshResponseSchema,
 )
 async def refresh_access_token(
